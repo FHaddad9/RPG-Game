c3e0a89b1db5c89234fc7e96545775c1dab282b1
Fhaddad9 <ferashaddadgit@gmail.com>
2022-04-06 15:55:14 +0100

    Basic map designed

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -12,12 +15,16 @@ public class TileManager {
 	GamePanel gp;
 	Tile[] tile;
 	
+	int mapTileNum[][];
+	
 	public TileManager(GamePanel gp) {
 		this.gp = gp;
 		
 		tile = new Tile[10];
+		mapTileNum = new int[gp.screenCol][gp.screenrow];
 		
 		getTileImage();
+		loadMap("/maps/land.txt");
 	}
 	
 	// Loads tile images
@@ -27,11 +34,50 @@ public class TileManager {
 			
 			tile[0] = new Tile();
 			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+			tile[1] = new Tile();
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
 			
 		} catch(IOException e) {
 			e.printStackTrace();
-		}
+		} 
 		
+	}
+	
+	public void loadMap(String map) {
+		
+		try {
+			
+			InputStream is = getClass().getResourceAsStream(map);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			
+			int col = 0;
+			int row = 0;
+			
+			while(col < gp.screenCol && row < gp.screenrow) {
+				
+				String line = br.readLine();
+				
+				while(col < gp.screenCol) {
+					
+					String nums[] = line.split(" ");
+					
+					int num = Integer.parseInt(nums[col]);
+					
+					mapTileNum[col][row] = num;
+					col++;
+				}
+				if(col == gp.screenCol) {
+					col = 0;
+					row++;
+				}
+			}
+			
+			br.close();
+			
+		} catch (Exception e) {
+			
+		}
 	}
 	
 	public void draw(Graphics2D g2) {
@@ -44,7 +90,9 @@ public class TileManager {
 		
 		while(col < gp.screenCol && row < gp.screenrow) {
 			
-			g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+			int tileNum = mapTileNum[col][row];
+			
+			g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
 			col++;
 			x += gp.tileSize;
 			
